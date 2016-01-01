64f1f7bc5fc376f3e179bf92d6e85e0a565842af
Alex Nabrozidis <nabrozidhs@gmail.com>
2016-01-01 21:48:32 +0100

    Finished level 10.

---
+++ src/level10/level10.kt
@@ -0,0 +1,23 @@
+package level10
+
+fun level10(s: String): String {
+    val sb = StringBuilder();
+    var count = 1
+    for (i in 0..(s.length - 2)) {
+        if (s[i] == s[i + 1]) {
+            count++
+        } else {
+            sb.append(count)
+            sb.append(s[i])
+            count = 1
+        }
+    }
+    sb.append(count)
+    sb.append(s[s.length - 1])
+    return sb.toString()
+}
+
+fun main(args : Array<String>) {
+    println((0..39).fold("3113322113", {acc, n -> level10(acc)}).length)
+    println((0..49).fold("3113322113", {acc, n -> level10(acc)}).length)
+}
