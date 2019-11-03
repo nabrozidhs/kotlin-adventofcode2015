8631bbbd551a7c9f7de40599d7720b840a3465e8
Alex <nabrozidhs@gmail.com>
2019-11-03 14:49:23 +0000

    day 13

---
+++ src/main/kotlin/level13/Day13.kt
@@ -0,0 +1,50 @@
+package level13
+
+import level09.permutations
+import java.io.File
+
+data class Person(val name: String, val mapping: Map<String, Int>)
+
+fun parse(input: List<String>): Map<String, Person> {
+    val mapping = mutableMapOf<String, Person>()
+
+    input.forEach { line ->
+        val split = line.split(" ")
+        val name = split[0]
+        val happiness = split[3].toInt()
+        val otherGuest = split[10].split(".")[0]
+        val isGain = split[2] == "gain"
+        val person = mapping.getOrPut(name) { Person(name, emptyMap()) }
+        mapping[name] = person.copy(
+            mapping = person.mapping.toMutableMap().apply {
+                put(otherGuest, if (isGain) happiness else -happiness)
+            }
+        )
+    }
+
+    return mapping
+}
+
+fun level13(guests: Map<String, Person>): Int =
+    guests.keys.toList().permutations().map { arrangement ->
+        arrangement.withIndex().map {
+            val guest = guests[it.value] ?: error("")
+            val left =
+                arrangement[(it.index - 1 + arrangement.size) % arrangement.size]
+            val right = arrangement[(it.index + 1) % arrangement.size]
+
+            guest.mapping.getOrDefault(left, 0) +
+                    guest.mapping.getOrDefault(right, 0)
+        }.sum()
+    }.max()!!
+
+fun main() {
+    val mapping = parse(File("data/level13/input.txt").readLines())
+
+    println("part1: ${level13(mapping)}")
+
+    val newMapping = mapping.toMutableMap().apply {
+        put("Myself", Person("Myself", emptyMap()))
+    }
+    println("part2: ${level13(newMapping)}")
+}
